8467d48b6b3524c4a3593a7cfebeed15aefb134c
MRskyPG <mrogalsky8113@gmail.com>
2023-09-14 18:20:03 +0700

    Add emperical functions (by V.T)

--- statistics.cpp
+++ statistics.cpp
@@ -7,24 +7,22 @@ double random() { return rand() / double(RAND_MAX); }
 // Theoretical distribution
 
 std::function<double(double)> cosine_power_density(double v) {
-  return [v](double x) {
-    return sqrt(acos(-1.0)) * tgamma(v / 2.0 + 1.0) *
-           pow(cos(acos(-1.0) * x / 2.0), v) / 2.0 / tgamma((v + 1.0) / 2.0);
-  };
+    return [v](double x) {
+        return sqrt(acos(-1.0)) * tgamma(v / 2.0 + 1.0) *
+               pow(cos(acos(-1.0) * x / 2.0), v) / 2.0 / tgamma((v + 1.0) / 2.0);
+    };
 }
 
-std::function<double(double)> shift_scaled_density(
-    std::function<double(double)> func, double mu, double lambda) {
-  if (lambda == 0) {
-    throw std::invalid_argument("Parameter lambda can't be equal zero");
-  }
-  return
-      [func, mu, lambda](double x) { return func((x - mu) / lambda) / lambda; };
+std::function<double(double)> shift_scaled_density(std::function<double(double)> func, double mu, double lambda) {
+    if (lambda == 0) {
+        throw std::invalid_argument("Parameter lambda can't be equal zero");
+    }
+    return
+            [func, mu, lambda](double x) { return func((x - mu) / lambda) / lambda; };
 }
 
-double cosine_power_mathematical_expectation(double v, double mu,
-                                             double lambda) {
-  return 0.0 + mu;
+double cosine_power_mathematical_expectation(double v, double mu, double lambda) {
+    return 0.0 + mu;
 }
 
 double cosine_power_mathematical_variance(double v, double mu, double lambda) {
@@ -35,61 +33,91 @@ double cosine_power_mathematical_variance(double v, double mu, double lambda) {
 }
 
 double cosine_power_mathematical_asymmetry(double v, double mu, double lambda) {
-  return 0.0;
+    return 0.0;
 }
 
 double cosine_power_mathematical_excess(double v, double mu, double lambda) {
-  return -polygamma((v + 2) / 2.0, 3) / 2.0 /
-         pow(polygamma((v + 2) / 2.0, 1), 2);
+    return -polygamma((v + 2) / 2.0, 3) / 2.0 /
+           pow(polygamma((v + 2) / 2.0, 1), 2);
 }
 
 // Emperical distribution
 
 double generate_cosine_power_value(double v) {
-  double r1 = random(), r2 = random();
-  return 2.0 * asin(sqrt(1 - pow(r1, 2.0 / v)) * cos(2.0 * acos(-1.0) * r2)) /
-         acos(-1.0);
+    double r1 = random(), r2 = random();
+    return 2.0 * asin(sqrt(1 - pow(r1, 2.0 / v)) * cos(2.0 * acos(-1.0) * r2)) /
+           acos(-1.0);
 }
 
-std::map<std::pair<double, double>, double> get_emperic_density(
-    std::vector<double>& data) {
-  if (data.size() == 0) {
-    throw std::invalid_argument("Data can't be empty");
-  }
-  int k = sturges_rule(data.size());
-  std::map<std::pair<double, double>, double> density;
-  double h = 2.0 / k;
-  for (int i = 1; i <= k; ++i) {
-    std::pair<double, double> key =
-        std::make_pair(-1.0 + (i - 1) * h, -1.0 + i * h);
-    density[key] = 0;
-  }
-  for (auto elem : data) {
-    for (int i = 1; i <= k; ++i) {
-      double left = -1.0 + (i - 1) * h, right = -1.0 + i * h;
-      if ((i != k) && (left <= elem) && (elem < right)) {
-        ++density[std::make_pair(left, right)];
-        break;
-      } else if ((i == k) && (left <= elem) && (elem <= right)) {
-        ++density[std::make_pair(left, right)];
-        break;
-      }
+double generate_shift_scaled_cosine_power_value(double v, double mu, double lambda) {
+    return mu+lambda*generate_cosine_power_value(v);
+}
+
+std::map<std::pair<double, double>, double> get_emperical_density(std::vector<double>& data) {
+    if (data.size() == 0) {
+        throw std::invalid_argument("Data can't be empty");
     }
-  }
-  for (auto key : density) {
-    density[key.first] /= double(h * data.size());
-  }
-  return density;
+    int k = sturges_rule(data.size());
+    double min_elem = *(std::min_element(data.begin(), data.end())), max_elem = *(std::max_element(data.begin(), data.end()));
+    std::map<std::pair<double, double>, double> density;
+    double h = (max_elem-min_elem) / k;
+    for (int i = 1; i <= k; ++i) {
+        std::pair<double, double> key = std::make_pair(min_elem + (i - 1) * h, min_elem + i * h);
+        density[key] = 0;
+    }
+    for (auto elem : data) {
+        for (int i = 1; i <= k; ++i) {
+            double left = min_elem + (i - 1) * h, right = min_elem + i * h;
+            if ((i != k) && (left <= elem) && (elem < right)) {
+                ++density[std::make_pair(left, right)];
+                break;
+            } else if ((i == k) && (left <= elem) && (elem <= right)) {
+                ++density[std::make_pair(left, right)];
+                break;
+            }
+        }
+    }
+    for (auto& key : density) {
+        density[key.first] /= double(h * data.size());
+    }
+    return density;
 }
 
 double emperical_moment(std::vector<double>& data, int p, bool central) {
-  if (data.size() == 0) {
-    throw std::length_error("Vector size must be higher then 0");
-  }
-  double mean = (!central) ? 0 : emperical_moment(data, 1, false);
-  double output = 0;
-  for (auto elem : data) {
-    output += pow(elem - mean, p);
-  }
-  return output / data.size();
+    if (size(data) == 0) {
+        throw std::length_error("Vector size must be higher then 0");
+    }
+    double mean = (!central) ? 0 : emperical_moment(data, 1, false);
+    double output = 0;
+    for (auto elem : data) {
+        output += pow(elem - mean, p);
+    }
+    return output / size(data);
+}
+
+double emperical_mathematical_expectation(std::vector<double>& data) {
+    return emperical_moment(data, 1, false);
+}
+
+double emperical_variance(std::vector<double>& data) {
+    return emperical_moment(data, 2, true);
+}
+
+double emperical_asymmetry(std::vector<double>& data) {
+    return emperical_moment(data, 3, true) / pow(emperical_moment(data, 2, true), 1.5);
+}
+
+double emperical_excess(std::vector<double>& data) {
+    return emperical_moment(data, 4, true) / pow(emperical_moment(data, 2, true), 2) - 3;
+}
+
+double generate_from_emperical_distribution(std::map<std::pair<double, double>, double> func) {
+    double val = random(), proba = 0;
+    for (auto& segment : func) {
+        proba += segment.second*(segment.first.second - segment.first.first);
+        if (val <= proba) {
+            double output = random() * (segment.first.second - segment.first.first) + segment.first.first;
+            return output;
+        }
+    }
 }
--- statistics.h
+++ statistics.h
@@ -5,6 +5,7 @@
 #include<vector>
 #include<stdexcept>
 #include<map>
+#include<algorithm>
 
 #include "smath.h"
 
@@ -19,9 +20,6 @@ std::function<double(double)> cosine_power_density(double v);
 
 // Returns shifted and scaled specified density (lambda != 0)
 std::function<double(double)> shift_scaled_density(std::function<double(double)> func, double mu, double lambda);
-
-// Returns emperical central/not-central mometh with p-s order
-double emperical_moment(std::vector<double>& data, int p, bool central);
 
 // Returns math expectation of cosine-power distribution with paramter v, shift coefficient mu, scale coefficient lambda
 double cosine_power_mathematical_expectation(double v, double mu, double lambda);
@@ -38,5 +36,34 @@ double cosine_power_mathematical_excess(double v, double mu, double lambda);
 // Generates random value from cosine_power distribution with parameter equal v
 double generate_cosine_power_value(double v);
 
+// Generate random value from shift-scaled cosine_power distribution with parameter v, mu, lambda
+double generate_shift_scaled_cosine_power_value(double v, double mu, double lambda);
+
+
+
+
+// Emperical functions
+
+
+
+
+// Returns emperical central/not-central mometh with p-s order
+double emperical_moment(std::vector<double>& data, int p, bool central);
+
+// Returns emperical mathematical expectation
+double emperical_mathematical_expectation(std::vector<double>& data);
+
+// Returns emperical vatiance
+double emperical_variance(std::vector<double>& data);
+
+// Returns emperical asymmetry coefficient
+double emperical_asymmetry(std::vector<double>& data);
+
+// Returns emperical excess
+double emperical_excess(std::vector<double>& data);
+
 // Returns emperic density (For each segment exclude last segment left border included right border excluded. Last segment includes boths borders)
-std::map<std::pair<double, double>, double> get_emperic_density(std::vector<double>& data);
+std::map<std::pair<double, double>, double> get_emperical_density(std::vector<double>& data);
+
+// Generates random value from emperical distribution
+double generate_from_emperical_distribution(std::map<std::pair<double, double>, double> func);
